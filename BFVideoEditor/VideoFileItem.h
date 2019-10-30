0ad7ab815e6629e1558002efa5dc06f20492d220
wubaofeng <17739094074@163.com>
2019-10-30 10:38:54 +0800

    提交Makefile

---
+++ BFVideoEditor/VideoFileItem.h
@@ -0,0 +1,17 @@
+//
+//  VideoFileItem.h
+//  MyVideoEditor
+//
+//  Created by friday on 2019/10/28.
+//  Copyright © 2019 friday. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface VideoFileItem : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BFVideoEditor/VideoFileItem.m
@@ -0,0 +1,13 @@
+//
+//  VideoFileItem.m
+//  MyVideoEditor
+//
+//  Created by friday on 2019/10/28.
+//  Copyright © 2019 friday. All rights reserved.
+//
+
+#import "VideoFileItem.h"
+
+@implementation VideoFileItem
+
+@end
